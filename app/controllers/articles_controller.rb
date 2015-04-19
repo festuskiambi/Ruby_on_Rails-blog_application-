23389fc9ff004a2313335ba0a8be9c3d9cc3193d
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-19 07:47:38 +0300

    now at the error checking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,22 @@
 class ArticlesController < ApplicationController
-	def new
-		end
-		def create
-  @article = Article.new(article_params)
- 
-  @article.save
-  redirect_to @article
-end
-def index
+	def index
     @articles = Article.all
   end
   def show
     @article = Article.find(params[:id])
   end
+	def new
+		end
+  def create
+  @article = Article.new(article_params)
+  if @article.save
+  redirect_to @article
+else
+	render 'new'
+end
+end
+
+  
  
 private
   def article_params
