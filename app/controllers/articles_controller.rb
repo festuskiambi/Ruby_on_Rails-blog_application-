b1cb4cb80af248c6b353fe6631626ec33bc860a4
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-19 08:46:39 +0300

    completed edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 	def new
+		@article = Article.new
 		end
+   def edit
+   @article = Article.find(params[:id])
+
+   end
   def create
   @article = Article.new(article_params)
   if @article.save
@@ -15,7 +20,14 @@ else
 	render 'new'
 end
 end
-
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
   
  
 private
